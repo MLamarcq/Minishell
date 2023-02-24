e0a7505b9f34f3e4d8665bd0878baac319a21d8e
labuche <mael.lamarcq0302@orange.fr>
2023-02-24 12:03:28 +0100

    add_lib_gael

---
+++ lib_gael/ft_strdup.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gael <gael@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 23:08:55 by gael              #+#    #+#             */
+/*   Updated: 2023/02/23 08:25:43 by gael             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_minishell.h"
+
+char	*ft_strdup(char *str)
+{
+	char	*res;
+	int		i;
+
+	res = malloc(sizeof (char) * (ft_strlen(str) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strdup_len(char *str, int start, int end)
+{
+	char	*res;
+	int		ite_str;
+	int		ite_res;
+
+	ite_res = 0;
+	ite_str = 0;
+	while (str[ite_str])
+		ite_str++;
+	if (end > ite_str)
+		return (NULL);
+	res = malloc(sizeof (char) * ((end - start) + 1));
+	if (!res)
+		return (NULL);
+	ite_str = start;
+	while (ite_str < end)
+	{
+		res[ite_res] = str[ite_str];
+		ite_res++;
+		ite_str++;
+	}
+	res[ite_res] = '\0';
+	return (res);
+}
---
+++ lib_gael/main.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gael <gael@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 15:26:23 by gael              #+#    #+#             */
+/*   Updated: 2023/02/24 11:34:38 by gael             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_minishell.h"
+
+//main mael
+// int main(int argc, char **argv)
+// {
+// 	(void)argc;
+// 	//(void)argv;
+// 	//char *input;
+
+// 	// while (1)
+// 	// {
+// 	// 	input = readline(">");
+// 	// 	// process 
+// 	// 	if (ft_strncmp(input, "exit", 4) == 0)
+// 	// 		break ;
+// 	// 	printf("input: %s\n", input);
+// 	// }
+// 	//printf("echo = %d\n", ft_echo(argv));
+// 	ft_echo(argv);
+// 	return (0);
+
+// }
+
+
+// main gael
+int	main(int argc, char *argv[], char **envp)
+{
+	t_mini_sh	mini_sh;
+	int	env_length;
+
+	// mini_sh.env = envp;
+	env_length = 0;
+	while (envp[env_length])
+		env_length++;
+	mini_sh.env = (char **)malloc((sizeof (char *)) * (env_length + 1));
+	env_length = -1;
+	while (envp[++env_length])
+	{
+		mini_sh.env[env_length] = ft_strdup(envp[env_length]);
+	}
+	mini_sh.env[env_length] = 0;
+	// while (1)
+	// {
+		mini_sh.output = NULL;
+		// ft_find_args(&mini_sh, "ls -l --color=never 	 -a | /usr/bin/grep -i --color=never  	   in >     result.txt");
+		// mini_sh.output = readline("\n\033[31m>\033[0m");
+		// printf(BLUE"mini_sh.output: %s\n"RESET, mini_sh.output);
+		// printf("mini_sh.output[5]-48: %i\n", mini_sh.output[5]-48);
+		// process 
+		ft_find_args(&mini_sh);
+	// 	if (ft_strncmp(mini_sh.output, "exit", 4) == 0)
+	// 	{
+	// 		char **str;
+	// 		int res;
+
+	// 		str = ft_split(mini_sh.output, ' ');
+	// 		res = atoi(str[1]);
+	// 		return (res);
+	// 	}
+	// }
+	// free_parsing(&mini_sh);
+	// free_env(&mini_sh);
+	// env_length = -1;
+	// while (mini_sh.env[++env_length])
+	// {
+	// 	free(mini_sh.env[env_length]);
+	// }
+	// free(mini_sh.env);
+	(void)mini_sh;
+	(void)argc;
+	(void)argv;
+	// return ((int)(mini_sh.output[5]));
+	printf("here\n");
+	return (0);
+}
